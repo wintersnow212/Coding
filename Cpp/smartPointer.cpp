8cae4fa3710c1dc793ad2ef2f7a11db8e5f4a0cd
wintersnow212 <kobesummerrain@gmail.com>
2019-07-17 01:14:10 -0400

    Update smartPointer.cpp

--- Cpp/smartPointer.cpp
+++ Cpp/smartPointer.cpp
@@ -8,12 +8,17 @@ class A
 public:
     A (int val):m_val(val)
     {
-        
+        cout << "constructor called" << endl;
     }
     
     A (const A& a)
     {
         cout << "Copy constructor called" << endl;
+    }
+    
+    ~A()
+    {
+        cout << "Destructor called" << endl;
     }
     
     int GetVal()
@@ -46,9 +51,9 @@ void MyFunc1(unique_ptr<A> arg)
 
 void uniqueTest1()
 {
-    unique_ptr<A> ptr = static_cast<unique_ptr<A>>(new A(1234));
-    // 使用make_unique更好
-    //unique_ptr<A> ptr = make_unique<A>(1234);
+    //unique_ptr<A> ptr = static_cast<unique_ptr<A>>(new A(1234));
+    // 使用make_unique更好 因为safety exception 
+    unique_ptr<A> ptr = make_unique<A>(1234);
     
     
     // unique_ptr delete the copy constructor 
@@ -56,7 +61,6 @@ void uniqueTest1()
     // use of deleted function ‘std::unique_ptr<_Tp, _Dp>::unique_ptr(const    
     // std::unique_ptr<_Tp, _Dp>&)
     //MyFunc1(ptr);
-    
     MyFunc1(move(ptr));
     
     // 这里的assert是成立的因为move就相当于把ownership给了MyFunc1的local copy了
@@ -70,14 +74,33 @@ void sharedTest1(shared_ptr<A> arg)
 {
     cout << arg->GetVal() << endl;
 }
-
+  
 // To execute C++, please define "int main()"
 int main() 
 {
     uniqueTest1();
     uniqueTest2();
     
+    // Reference counting 
+    // make shared --- allocate control block of memory for reference count
+    // construct them together
+    cout << "--------Shared pointer------------" << endl;
+    
+    // allocate a Resource object and have it owned by std::shared_ptr
+
     shared_ptr<A> sharedPtr = make_shared<A>(5678);
+    {
+        // use copy initialization to make another std::shared_ptr pointing to the same thing
+        std::shared_ptr<A> ptr2(sharedPtr); 
+        
+
+        // Weak pointer will not increase reference count
+        // this is just like the reference but its memory is not persist
+        std::weak_ptr<A> weakPtr = ptr2;    
+            
+    } // ptr2 goes out of scope here, but nothing happens
+ 
     sharedTest1(sharedPtr);
+    cout << "--------Shared pointer------------" << endl;
     return 0;
 }
