196867a0036c58b264c5e6533bbd386673415853
wintersnow <kobesummerrain@gmail.com>
2019-07-21 10:06:53 -0400

    Update smartPointerImp.cpp

--- Cpp/smartPointerImp.cpp
+++ Cpp/smartPointerImp.cpp
@@ -3,6 +3,41 @@
 #include <memory>
 using namespace std;
 
+// Give the ownership of heap allocated memory to stack varible
+// so that when it is out of scope, it will be freed autonmatically
+template<typename T>
+class UniquePtr
+{
+public:
+    UniquePtr()
+        : m_ptr(nullptr)
+    {
+        
+    }
+    
+    // This is actual a shadow copy 
+    // but it is ok because the point here is let m_Ptr share the
+    // reference of ptr so that we can free their shared memory
+    UniquePtr(T* ptr)
+        : m_ptr(ptr)
+    {
+        
+    }
+    
+    ~UniquePtr()
+    {
+        delete m_ptr;
+    }
+    
+    
+    UniquePtr (const UniquePtr<T>& other) = delete;
+    
+    UniquePtr<T>& operator= (const UniquePtr<T>& other) = delete;
+    
+private:
+    T* m_ptr;
+};
+
 
 class RefCnt
 {
@@ -139,7 +174,7 @@ public:
     
     AirCraft(const string& modelName) :m_Model(modelName)
     {
-        cout << "Aircraft type" << m_Model << "is created!" << endl;
+        cout << "Aircraft type " << m_Model << "is created!" << endl;
     }
       
     void SetAirCraftModel(const string& modelName)
@@ -192,88 +227,92 @@ int main()
     // Ref count for hornett is 2. "B2 Spirit" is destroyed.
     rP = hP; 
    
+    // 这里这样是不会call copy constructor 或者copy assignment
+    UniquePtr<AirCraft> uP = new AirCraft("Unique F-22 Tiger");
     
+    // unique pointer是不能copy的 因为是unqiue ownership啊
+    //UniquePtr<AirCraft> uPCopy = uP;
     return 0;
 }
 
-template<typename T>
-class SharedPtrSp
-{
-public:
-    SharedPtrSp()
-        : m_ptr(nullptr)
-        , m_refCnt(0)   
-    {
+// template<typename T>
+// class SharedPtrSp
+// {
+// public:
+//     SharedPtrSp()
+//         : m_ptr(nullptr)
+//         , m_refCnt(0)   
+//     {
         
-    }
+//     }
     
-    SharedPtrSp(T* ptr) 
-        : m_ptr(ptr)
-        , m_refCnt(1)
-    {
-        cout << "Created smart_ptr! Ref count is " << m_refCnt << endl;
-    }
+//     SharedPtrSp(T* ptr) 
+//         : m_ptr(ptr)
+//         , m_refCnt(1)
+//     {
+//         cout << "Created smart_ptr! Ref count is " << m_refCnt << endl;
+//     }
     
-    virtual ~SharedPtrSp()
-    {
-        if (m_refCnt > 0)
-        {
-            m_refCnt--;
-            cout << "Destroyed smart_ptr! Ref count is " << m_refCnt<< endl;
-            if (m_refCnt == 0)
-            {
-                delete m_ptr;
-                m_ptr = nullptr;
-            }
-        }
-    }
+//     virtual ~SharedPtrSp()
+//     {
+//         if (m_refCnt > 0)
+//         {
+//             m_refCnt--;
+//             cout << "Destroyed smart_ptr! Ref count is " << m_refCnt<< endl;
+//             if (m_refCnt == 0)
+//             {
+//                 delete m_ptr;
+//                 m_ptr = nullptr;
+//             }
+//         }
+//     }
     
-    // Copy constructor
-    SharedPtrSp (const SharedPtrSp& other)
-    {
-        m_ptr = other.m_ptr;
-        m_refCnt = other.m_refCnt;
-        //m_refCnt->Increment();
-        m_refCnt++;
+//     // Copy constructor
+//     SharedPtrSp (const SharedPtrSp& other)
+//     {
+//         m_ptr = other.m_ptr;
+//         m_refCnt = other.m_refCnt;
+//         //m_refCnt->Increment();
+//         m_refCnt++;
         
-        cout << "Copy constructor called! Ref count is " << m_refCnt << endl;
-    }
+//         cout << "Copy constructor called! Ref count is " << m_refCnt << endl;
+//     }
     
-    // 这里template的return是SharedPtr<T>
-    // Copy assignment operator
-    SharedPtrSp<T>& operator= (const SharedPtrSp& other)
-    {
-        // 不要写成*this == other !!!!
-        // 因为那样没有 对应的operator!!!
-        if (this == &other)
-        {
-            return *this;
-        }
+//     // 这里template的return是SharedPtr<T>
+//     // Copy assignment operator
+//     SharedPtrSp<T>& operator= (const SharedPtrSp& other)
+//     {
+//         // 不要写成*this == other !!!!
+//         // 因为那样没有 对应的operator!!!
+//         if (this == &other)
+//         {
+//             return *this;
+//         }
         
-        if (m_ptr != nullptr)
-            delete m_ptr;
-        m_ptr = other.m_ptr;
-        m_refCnt = other.m_refCnt;
-        m_refCnt++;
+//         if (m_ptr != nullptr)
+//             delete m_ptr;
+//         m_ptr = other.m_ptr;
+//         m_refCnt = other.m_refCnt;
+//         m_refCnt++;
         
-        cout << "Copy assignment called! Ref count is "
-             << m_refCnt << endl;
-        return *this;
-    }
+//         cout << "Copy assignment called! Ref count is "
+//              << m_refCnt << endl;
+//         return *this;
+//     }
     
-    // overload operator ->
-    T* operator-> ()
-    {
-        return m_ptr;
-    }
+//     // overload operator ->
+//     T* operator-> ()
+//     {
+//         return m_ptr;
+//     }
     
-    T& operator* ()
-    {
-        return *m_ptr;
-    }
+//     T& operator* ()
+//     {
+//         return *m_ptr;
+//     }
     
     
-private:
-    T*  m_ptr;
-    int m_refCnt;
-};
+// private:
+//     T*  m_ptr;
+//     int m_refCnt;
+// };
