eb54bcb3b5cf6ba8bee34f346fb770d599ea845d
wintersnow212 <kobesummerrain@gmail.com>
2019-07-29 16:57:36 -0400

    Update rope.cpp

--- Cpp/rope.cpp
+++ Cpp/rope.cpp
@@ -4,7 +4,8 @@ using namespace std;
 一个Rope 数据结构用来高效的处理字符串的拼接、查询、删除、及随机访问。
 Rope的一个典型应用场景是：在一个文本编辑程序里，用来保存较长的文本字符串。如下图，字符串“Hello_my_name_is_Simon”的表示 
 One of the most common operations on strings is appending or concatenation. 
-Appending to the end of a string when the string is stored in the traditional manner (i.e. an array of characters) would take a minimum of O(n) time (where n is the length of the original string).
+Appending to the end of a string when the string is stored in the traditional manner 
+(i.e. an array of characters) would take a minimum of O(n) time (where n is the length of the original string).
 
 We can reduce time taken by append using Ropes Data Structure.
 
@@ -22,7 +23,91 @@ string in the data field (according to the C0 string library function string_len
 ropes, and if it has a len field equal to the sum of the len field of its children. The
 data field of a non-leaf is unspecificed. We'll call these non-leaves concatenation nodes.
 
+
+
+Index
+
+Figure 2.1: Example of index lookup on a rope.
+Definition: Index(i): return the character at position i
+Time complexity: {\displaystyle O(\log N)} O(\log N)
+To retrieve the i-th character, we begin a recursive search from the root node:
+
+function index(RopeNode node, integer i)
+    if node.weight < i and exists(node.right) then
+        return index(node.right, i - node.weight)
+    end
+    
+    if exists(node.left) then
+        return index(node.left, i)
+    end
+    
+    return node.[i]string
+end
+For example, to find the character at i=10 in Figure 2.1 shown on the right, start at the root node (A), find that 22 is greater than 10 and there is a left child, so go to the left child (B). 9 is less than 10, so subtract 9 from 10 (leaving i=1) and go to the right child (D). Then because 6 is greater than 1 and there's a left child, go to the left child (G). 2 is greater than 1 and there's a left child, so go to the left child again (J). Finally 2 is greater than 1 but there is no left child, so the character at index 1 of the short string "na", is the answer.
+
+Concat
+
+Figure 2.2: Concatenating two child ropes into a single rope.
+Definition: Concat(S1, S2): concatenate two ropes, S1 and S2, into a single rope.
+Time complexity: {\displaystyle O(1)} O(1) (or {\displaystyle O(\log N)} O(\log N) time to compute the root weight)
+A concatenation can be performed simply by creating a new root node with left = S1 and right = S2, which is constant time. The weight of the parent node is set to the length of the left child S1, which would take {\displaystyle O(\log N)} O(\log N) time, if the tree is balanced.
+
+As most rope operations require balanced trees, the tree may need to be re-balanced after concatenation.
+
+Split
+
+Figure 2.3: Splitting a rope in half.
+Definition: Split (i, S): split the string S into two new strings S1 and S2, S1 = C1, …, Ci and S2 = Ci + 1, …, Cm.
+Time complexity: {\displaystyle O(\log N)} O(\log N)
+There are two cases that must be dealt with:
+
+The split point is at the end of a string (i.e. after the last character of a leaf node)
+The split point is in the middle of a string.
+The second case reduces to the first by splitting the string at the split point to create two new leaf nodes, then creating a new node that is the parent of the two component strings.
+
+For example, to split the 22-character rope pictured in Figure 2.3 into two equal component ropes of length 11, query the 12th character to locate the node K at the bottom level. Remove the link between K and G. Go to the parent of G and subtract the weight of K from the weight of D. Travel up the tree and remove any right links to subtrees covering characters past position 11, subtracting the weight of K from their parent nodes (only node D and A, in this case). Finally, build up the newly orphaned nodes K and H by concatenating them together and creating a new parent P with weight equal to the length of the left node K.
+
+As most rope operations require balanced trees, the tree may need to be re-balanced after splitting.
+
+Insert
+Definition: Insert(i, S’): insert the string S’ beginning at position i in the string s, to form a new string C1, …, Ci, S', Ci + 1, …, Cm.
+Time complexity: {\displaystyle O(\log N)} O(\log N).
+This operation can be done by a Split() and two Concat() operations. The cost is the sum of the three.
+
+Delete
+Definition: Delete(i, j): delete the substring Ci, …, Ci + j − 1, from s to form a new string C1, …, Ci − 1, Ci + j, …, Cm.
+Time complexity: {\displaystyle O(\log N)} O(\log N).
+This operation can be done by two Split() and one Concat() operation. First, split the rope in three, divided by i-th and i+j-th character respectively, which extracts the string to delete in a separate node. Then concatenate the other two nodes.
+
+Report
+Definition: Report(i, j): output the string Ci, …, Ci + j − 1.
+Time complexity: {\displaystyle O(j+\log N)} {\displaystyle O(j+\log N)}
+To report the string Ci, …, Ci + j − 1, find the node u that contains Ci and weight(u) >= j, and then traverse T starting at node u. Output Ci, …, Ci + j − 1 by doing an in-order traversal of T starting at node u.
+
+
+
 */
+//https://github.com/wroever/rope/tree/master/src/proj
+
+
+/*
+
+Insert
+Definition: Insert(i, S’): insert the string S’ beginning at position i in the string s, to form a new string C1, …, Ci, S', Ci + 1, …, Cm.
+Time complexity: {\displaystyle O(\log N)} O(\log N).
+This operation can be done by a Split() and two Concat() operations. The cost is the sum of the three.
+
+Delete
+Definition: Delete(i, j): delete the substring Ci, …, Ci + j − 1, from s to form a new string C1, …, Ci − 1, Ci + j, …, Cm.
+Time complexity: {\displaystyle O(\log N)} O(\log N).
+This operation can be done by two Split() and one Concat() operation. First, split the rope in three, divided by i-th and i+j-th character respectively, which extracts the string to delete in a separate node. Then concatenate the other two nodes.
+
+Report
+Definition: Report(i, j): output the string Ci, …, Ci + j − 1.
+Time complexity: {\displaystyle O(j+\log N)} {\displaystyle O(j+\log N)}
+To report the string Ci, …, Ci + j − 1, find the node u that contains Ci and weight(u) >= j, and then traverse T starting at node u. Output Ci, …, Ci + j − 1 by doing an in-order traversal of T starting at node u.
+
+ */
 
 
 typedef struct rope_node rope;
@@ -328,13 +413,254 @@ string rope_node::getSubstring(size_t start, size_t len) const
 } // namespace proj
 
 
+namespace proj
+{
+  using std::make_unique;
+  using std::pair;
+  
+  // out-of-bounds error constant
+  std::invalid_argument ERROR_OOB_ROPE = std::invalid_argument("Error: string index out of bounds");
 
-
-// To execute C++, please define "int main()"
-int main() {
-  auto words = { "Hello, ", "World!", "\n" };
-  for (const string& word : words) {
-    cout << word;
+  // Default constructor - produces a rope representing the empty string
+  rope::rope(void) : rope("")
+  {}
+  
+  // Construct a rope from the given string
+  rope::rope(const string& str) {
+    this->root_ = make_unique<rope_node>(str);
   }
-  return 0;
+  
+  // Copy constructor
+  rope::rope(const rope& r) {
+    rope_node newRoot = rope_node(*r.root_);
+    this->root_ = make_unique<rope_node>(newRoot);
+  }
+  
+  // Get the string stored in the rope
+  string rope::toString(void) const {
+    if(this->root_ == nullptr)
+      return "";
+    return this->root_->treeToString();
+  }
+  
+  // Get the length of the stored string
+  size_t rope::length(void) const {
+    if(this->root_ == nullptr)
+      return 0;
+    return this->root_->getLength();
+  }
+  
+  // Get the character at the given position in the represented string
+  char rope::at(size_t index) const {
+    if(this->root_ == nullptr)
+      throw ERROR_OOB_ROPE;
+    return this->root_->getCharByIndex(index);
+  }
+
+  // Return the substring of length (len) beginning at the specified index
+  string rope::substring(size_t start, size_t len) const {
+    size_t actualLength = this->length();
+    if (start > actualLength || (start+len) > actualLength) throw ERROR_OOB_ROPE;
+    return this->root_->getSubstring(start, len);
+  }
+
+  // Insert the given string into the rope, beginning at the specified index (i)
+  void rope::insert(size_t i, const string& str) {
+    this->insert(i,rope(str));
+  }
+
+  // Insert the given rope into the rope, beginning at the specified index (i)
+  void rope::insert(size_t i, const rope& r) {
+    if (this->length() < i) {
+      throw ERROR_OOB_ROPE;
+    } else {
+      rope tmp = rope(r);
+      pair<handle, handle> origRopeSplit = splitAt(move(this->root_),i);
+      handle tmpConcat = make_unique<rope_node>(move(origRopeSplit.first), move(tmp.root_));
+      this->root_ = make_unique<rope_node>(move(tmpConcat), move(origRopeSplit.second));
+    }
+  }
+  
+  // Append the argument to the existing rope
+  void rope::append(const string& str) {
+    rope tmp = rope(str);
+    this->root_ = make_unique<rope_node>(move(this->root_), move(tmp.root_));
+  }
+
+  // Append the argument to the existing rope
+  void rope::append(const rope& r) {
+    rope tmp = rope(r);
+    this->root_ = make_unique<rope_node>(move(this->root_), move(tmp.root_));
+  }
+  
+  // Delete the substring of (len) characters beginning at index (start)
+  void rope::rdelete(size_t start, size_t len) {
+    size_t actualLength = this->length();
+    if (start > actualLength || start+len > actualLength) {
+      throw ERROR_OOB_ROPE;
+    } else {
+      pair<handle, handle> firstSplit = splitAt(move(this->root_),start);
+      pair<handle, handle> secondSplit = splitAt(move(firstSplit.second),len);
+      secondSplit.first.reset();
+      this->root_ = make_unique<rope_node>(move(firstSplit.first), move(secondSplit.second));
+    }
+  }
+  
+  // Determine if rope is balanced
+  //
+  // A rope is balanced if and only if its length is greater than or equal to
+  //   fib(d+2) where d refers to the depth of the rope and fib(n) represents
+  //   the nth fibonacci number i.e. in the set {1,1,2,3,5,8,etc...}
+  bool rope::isBalanced(void) const{
+    if(this->root_ == nullptr)
+      return true;
+    size_t d = this->root_->getDepth();
+    return this->length() >= fib(d+2);
+  }
+  
+  // Balance a rope
+  void rope::balance(void) {
+    // initiate rebalancing only if rope is unbalanced
+    if(!this->isBalanced()) {
+      // build vector representation of Fibonacci intervals
+      std::vector<size_t> intervals = buildFibList(this->length());
+      std::vector<handle> nodes(intervals.size());
+      
+      // get leaf nodes
+      std::vector<rope_node *> leaves;
+      this->root_->getLeaves(leaves);
+      
+      size_t i;
+      size_t max_i = intervals.size()-1;
+      size_t currMaxInterval = 0;
+      handle acc = nullptr;
+      handle tmp = nullptr;
+
+      // attempt to insert each leaf into nodes vector based on length
+      for (auto& leaf : leaves) {
+        size_t len = leaf->getLength();
+        bool inserted = false;
+        
+        // ignore empty leaf nodes
+        if (len > 0) {
+          acc = make_unique<rope_node>(*leaf);
+          i = 0;
+
+          while(!inserted) {
+            // find appropriate slot for the acc node to be inserted,
+            // concatenating with nodes encountered along the way
+            while(i < max_i && len >= intervals[i+1]) {
+              if(nodes[i].get() != nullptr) {
+                // concatenate encountered entries with node to be inserted
+                tmp = make_unique<rope_node>(*nodes[i].get());
+                acc = make_unique<rope_node>(*acc.get());
+                acc = make_unique<rope_node>(move(tmp),move(acc));
+                
+                // update len
+                len = acc->getLength();
+                
+                // if new length is sufficiently great that the node must be
+                //   moved to a new slot, we clear out the existing entry
+                if(len >= intervals[i+1]) nodes[i] = nullptr;
+              }
+              i++;
+            }
+            
+            // target slot found -- check if occupied
+            if (nodes[i].get() == nullptr) {
+              nodes[i].swap(acc);
+              inserted = true;
+              // update currMaxInterval if necessary
+              if(i > currMaxInterval) currMaxInterval = i;
+            } else {
+              // concatenate encountered entries with node to be inserted
+              tmp = make_unique<rope_node>(*nodes[i].get());
+              acc = make_unique<rope_node>(*acc.get());
+              acc = make_unique<rope_node>(move(tmp),move(acc));
+              
+              // update len
+              len = acc->getLength();
+              
+              // if new length is sufficiently great that the node must be
+              //   moved to a new slot, we clear out the existing entry
+              if(len >= intervals[i+1]) nodes[i] = nullptr;
+            }
+          }
+        }
+      }
+      
+      // concatenate remaining entries to produce balanced rope
+      acc = move(nodes[currMaxInterval]);
+      for(int idx = currMaxInterval; idx >= 0; idx--) {
+        if(nodes[idx] != nullptr) {
+          tmp = make_unique<rope_node>(*nodes[idx].get());
+          acc = make_unique<rope_node>(move(acc),move(tmp));
+        }
+      }
+      this->root_ = move(acc); // reset root
+      
+    }
+  }
+  
+  // Assignment operator
+  rope& rope::operator=(const rope& rhs) {
+    // check for self-assignment
+    if(this == &rhs) return *this;
+    // delete existing rope to recover memory
+    this->root_.reset();
+    // invoke copy constructor
+    this->root_ = make_unique<rope_node>(*(rhs.root_.get()));
+    return *this;
+  }
+  
+  // Determine if two ropes contain identical strings
+  bool rope::operator ==(const rope& rhs) const {
+    return this->toString() == rhs.toString();
+  }
+  
+  // Determine if two ropes contain identical strings
+  bool rope::operator !=(const rope& rhs) const {
+    return !(*this == rhs);
+  }
+  
+  // Print the rope
+  std::ostream& operator<<(std::ostream& out, const rope& r) {
+    return out << r.toString();
+  }
+  
+  
+  // Compute the nth Fibonacci number, in O(n) time
+  size_t fib(size_t n) {
+    // initialize first two numbers in sequence
+    int a = 0, b = 1, next;
+    if(n == 0) return a;
+    for (size_t i = 2; i <= n; i++) {
+      next = a + b;
+      a = b;
+      b = next;
+    }
+    return b;
+  };
+  
+  // Construct a vector where the nth entry represents the interval between the
+  //   Fibonacci numbers F(n+2) and F(n+3), and the final entry includes the number
+  //   specified in the length parameter
+  // e.g. buildFibList(0) -> {}
+  //      buildFibList(1) -> {[1,2)}
+  //      buildFibList(8) -> {[1,2),[2,3),[3,5),[5,8),[8,13)}
+  std::vector<size_t> buildFibList(size_t len) {
+    // initialize a and b to the first and second fib numbers respectively
+    int a = 0, b = 1, next;
+    std::vector<size_t> intervals = std::vector<size_t>();
+    while (a <= len) {
+      if (a > 0) {
+        intervals.push_back(b);
+      }
+      next = a + b;
+      a = b;
+      b = next;
+    }
+    return intervals;
+  }
+  
 }
