e7d95ebdd425c6564438216a5e36135e9c9d73cb
wintersnow <kobesummerrain@gmail.com>
2019-09-02 23:41:41 -0700

    Update hashFunction.c

--- C/hashFunction.c
+++ C/hashFunction.c
@@ -1,3 +1,17 @@
+/*
+hashcode("abcd") = (ascii(a) * 333 + ascii(b) * 332 + ascii(c) *33 + ascii(d)) % HASH_SIZE 
+
+                              = (97* 333 + 98 * 332 + 99 * 33 +100) % HASH_SIZE
+
+                              = 3595978 % HASH_SIZE
+
+here HASH_SIZE is the capacity of the hash table (you can assume a hash table is like an array with index 0 ~ HASH_SIZE-1).
+
+
+
+*/
+
+
 int hashCode(char* key, int HASH_SIZE) {
     // write your code here
     int ans = 0;
