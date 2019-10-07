381e60c8d28b7a248fccaa8f4f65de5b1943e503
wintersnow212 <kobesummerrain@gmail.com>
2019-10-06 22:38:27 -0400

    Create evalator.cpp

---
+++ Cpp/evalator.cpp
@@ -0,0 +1,13 @@
+Evalator.cpp
+https://blog.csdn.net/qq508618087/article/details/53306244
+https://cloud.tencent.com/developer/article/1102760   这个还不错把 
+https://www.jiuzhang.com/qa/129/
+https://leetcode.com/discuss/interview-question/124660/design-an-Elevator
+
+1. 关键是怎么处理request 是按照fifo 还是同一方向就近 还是不管方向的看谁更近
+2. 几个基本函数
+
+openDoor
+closeDoor
+MoveUp
+MoveDown
