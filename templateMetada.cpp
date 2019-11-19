29aff388dad45a3149518e77614e1f71c0ad0422
wintersnow <kobesummerrain@gmail.com>
2019-11-18 21:29:12 -0500

    Update templateMetada.cpp

--- templateMetada.cpp
+++ templateMetada.cpp
@@ -1,142 +1,32 @@
 #include <iostream>
-
-/****************************************************************
-*  Non-type template parameter
-****************************************************************/
-template<typename T, int size>
-class StaticArray
-{
-private:
-    // The non-type parameter controls the size of the array
-    T m_array[size];
- 
-public:
-    T* getArray();
-    
-    T& operator[](int index)
-    {
-        return m_array[index];
-    }
-};
- 
-// Showing how a function for a class with a non-type parameter is defined outside of the class
-template <class T, int size>
-T* StaticArray<T, size>::getArray()
-{
-    return m_array;
-}
-
-
-/****************************************************************************
-*     typename keyword vs class keyword
-*  Unlike type template parameter declaration, template template 
-*  parameter 
-*  declaration can only use the keyword class and not typename.
-* (until C++17)
-* The first one is in the case of dependent types. 
-* typename is used to declare when you are referencing a nested type that 
-* depends on another template parameter, such as the typedef in this example:
-* 
-* template<typename param_t>
-* class Foo
-* {
-*     typedef typename param_t::baz sub_t;
-* };
-* The second one you actually show in your question, though you might not realize it:
-* 
-* template <template<typename, typename> class Container, typename Type>
-* When specifying a template template, the class keyword MUST be used as above -- 
-* it is not interchangeable with typename in this case (note: since C++17 both keywords are allowed in this case).
-* 
-* You also must use class when explicitly instantiating a template:
-
-template class Foo<int>;
-**********************************************************************/
-
-/****************************************************************
-*  template template parameter
-****************************************************************/
-
-
-
-/****************************************************************
-*        Template specilalization
-*  所谓模板特例化即对于通例中的某种或某些情况做单独专门实现，
-*  最简单的情况是对每个模板参数指定一个具体值，这成为完全特例化（full specialization).
-*  另外，可以限制模板参数在一个范围取值或满足一定关系等，这称为部分特例化
-* （partial specialization
-****************************************************************/
-
-// However if the function’s observable behavior is consistent for all the T types with the differences 
-// limited to implementation details, then you can proceed. 
-// Let’s proceed with an example of this (conceptual only; not C++):
-template<typename T>
-void foo(const T& x)
-{
-  switch (typeof(T)) {  // Conceptual only; not C++
-    case int:
-      // ...implementation details when T is int
-      break;
-    case std::string:
-      // ...implementation details when T is std::string
-      break;
-    default:
-      // ...implementation details when T is neither int nor std::string
-      break;
-  }
-}
-
-// One way to implement the above is via template specialization. Instead of a switch-statement, 
-// you end up breaking up the code into separate functions. 
-// The first function is the default case — the code to be used when T is anything other than int or std::string:
-template<typename T>
-void foo(const T& x)
-{
-  // ...implementation details when T is neither int nor std::string
-}
-
-// 下面的这两个完全specialization后 我们遇到这样的type直接就进来了！！！！
-// Next are the two specializations, first for the int case…
-template<>
-void foo<int>(const int& x)
-{
-  // ...implementation details when T is int
-}
-
-// …and next for the std::string case…
-template<>
-void foo<std::string>(const std::string& x)
-{
-  // ...implementation details when T is std::string
-}
-//That’s it; you’re done. The compiler will automagically select the correct specialization when it sees which T you are using.
-
-// 这个是模板型模板参数； 这里一定要用class 而不是template
-template<typename T, int i> class cp00; 
-
-// 通例
-template<typename T1, typename T2, int i, template<typename, int> class CP>
-class TMP;
-
-// 完全特例化specilalization 全部都是直接输入
-template<>
-class TMP<int, float, 2, cp00>;
-
-// 第一个参数有const修饰
-template<typename T1, typename T2, int i, template<typename, int> class CP>
-class TMP<const T1, T2, i, CP>;
+using namespace std;
 
 /****************************************************************
  * template meta programming TMP的例子
  * http://www.cs.rpi.edu/~musser/design/blitz/meta-art.html
  * 好处： The use of templates can be thought of as compile-time execution
- *  so it is easier to be optimized by the compiler
+ *  Move computations from run-time to compile-time
+ *  Types of metaprograms
+    1.Compile-time numeric calculations
+    2.Code generation using numeric calculations 
+    3.“Self-adapting” code
+    4.Type manipulations
+    5.Checking types for certain properties — Converting types to other types
  ****************************************************************/
-// non-typed template   
+
+
+/****************************************************************
+Compile-time numeric calculations
+****************************************************************/
 template <unsigned int n>
-struct factorial {
-    enum { value = n * factorial<n - 1>::value };
+struct factorial 
+{
+    enum 
+    { 
+        value = n * factorial<n - 1>::value 
+    };
 };
+
 // 完全实例化
 template <> // The following is a template class with no templated parameters
 struct factorial<0>  // template specialization 完全特例化
@@ -145,7 +35,6 @@ struct factorial<0>  // template specialization 完全特例化
 };
 
 /****************************************************************
-
 if (condition) 
    statement1;
 else 
@@ -155,7 +44,6 @@ else
 template<bool C>
 class _name 
 { 
-    
 };
 
 template<>
@@ -173,31 +61,51 @@ public:
     { std::cout << "FALSE" << std::endl; }         // false case
 };
 
+
+/****************************************************************
+ Small types check utility
+****************************************************************/
+template <typename T> 
+struct SizeOfT 
+{ 
+    enum { value = sizeof(T) };
+};
+
+
+// template<typename T> struct is_pointer_helper     : std::false_type {};
+// template<typename T> struct is_pointer_helper<T*> : std::true_type {};
+// template<typename T> struct my_is_pointer : is_pointer_helper<typename std::remove_cv<T>::type> {};
+
+template <typename T>
+struct is_pointer_type
+{
+    enum { value = false };
+};
+
+template <typename T>
+struct is_pointer_type<T*>
+{
+    enum { value = true };
+};
+
+template <typename T>
+bool my_is_pointer(const T&)
+{
+    return is_pointer_type<T>::value;
+}
+
 int main()
 {
+    typedef int* A;
+    cout << "Check if it is the pointer type " << is_pointer_type<A>::value << endl;
+    
     // Replacement for 'if/else' statement:
     const bool flag = true;
     _name<flag == true>::f();
-    std::cout << factorial<4>::value << std::endl;
     
-     // declare an integer array with room for 12 integers
-    StaticArray<int, 12> intArray;
- 
-    // Fill it up in order, then print it backwards
-    for (int count=0; count < 12; ++count)
-        intArray[count] = count;
- 
-    for (int count=11; count >= 0; --count)
-        std::cout << intArray[count] << " ";
-    std::cout << '\n';
- 
-    // declare a double buffer with room for 4 doubles
-    StaticArray<double, 4> doubleArray;
- 
-    for (int count=0; count < 4; ++count)
-        doubleArray[count] = 4.4 + 0.1*count;
- 
-    for (int count=0; count < 4; ++count)
-        std::cout << doubleArray[count] << ' ';
-     return 0;
+    
+    cout << SizeOfT<int>::value << endl;
+    
+    
+    std::cout << factorial<4>::value << std::endl;
 }
