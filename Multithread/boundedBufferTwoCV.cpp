aa42669d9b2f9c95f75c9f732574dcf87948653b
wintersnow <kobesummerrain@gmail.com>
2019-12-02 00:30:05 -0500

    Update boundedBufferTwoCV.cpp

--- Multithread/boundedBufferTwoCV.cpp
+++ Multithread/boundedBufferTwoCV.cpp
@@ -46,7 +46,7 @@ SyncStream<Stream> synchronized(Stream &stream)
     return SyncStream<Stream>(stream);
 }
  
-
+template<int cap>
 struct BoundedBuffer {
     int* buffer;
     int capacity;
@@ -55,7 +55,7 @@ struct BoundedBuffer {
     int rear;
     int count;
 
-    std::mutex lock;
+    std::mutex _mutex;
 
     // 这里conditional varaible 有两个！！！
     std::condition_variable not_full;
@@ -75,7 +75,7 @@ struct BoundedBuffer {
     }
 
     void push(int data){
-        std::unique_lock<std::mutex> l(lock);
+        std::unique_lock<std::mutex> l(_mutex);
 
         while (count == capacity)
         {
@@ -93,10 +93,25 @@ struct BoundedBuffer {
     }
 
     int pop(){
-        std::unique_lock<std::mutex> l(lock);
-
+    
+        // 所有的lock都是build around the underlying  mutex
+        std::unique_lock<std::mutex> l(_mutex);
+        // 这里用while loop 而不是if 也解决了spurious wakeup issue
+        // spurious wakeup : a thread might be awoken from its waiting state even though no 
+        // thread signaled the condition variable
+        // 所以我们需要while loop 再检查一遍
+        // https://www.justsoftwaresolutions.co.uk/threading/condition-variable-spurious-wakes.html
+        // https://github.com/angrave/SystemProgramming/wiki/Synchronization,-Part-5:-Condition-Variables
         while (count == 0)
         {
+            /* 
+          这里的 
+          1. 首先 我们在这wait了 就会立马release mutex 不然别人怎么notify你呢
+          2. suspend the execution of the thread.
+          3. When the condition variable is notified, the mutex is atomically reacquired
+          4. 在出function scope destory lock object的时候 If the object currently owns a lock 
+             on the managed mutex object,its unlock member is called before destroying object.
+          */
             not_empty.wait(l);
         }
         //not_empty.wait(l, [this](){return count != 0; });
@@ -104,7 +119,17 @@ struct BoundedBuffer {
         int result = buffer[front];
         front = (front + 1) % capacity;
         --count;
-
+        /* why the “pop”s don’t need “mlock.unlock();
+          All unlocking is taken care of by the condition_variable’s wait function and by the unique_lock’s destructor.
+        Manual unlocking is done before notifying, to avoid waking up the waiting thread only to block again 
+        因为这里not_full wait的时候在等待这里的lock 释放了  
+        the waiting thread might wake up immediately it is notified, and before the mutex is     
+        unlocked, in which case it will have to block when the mutex is reacquired on the exit 
+        from wait. By rewriting the function so that the notification comes after the mutex is 
+        unlocked, the waiting thread will be able to acquire the mutex without blocking
+        也就是如果不manually unlock的话 wait thread 在被notify后准备re-acquire lock时会被迫等待！！
+      */
+  
         l.unlock();
         not_full.notify_one();
 
@@ -112,7 +137,8 @@ struct BoundedBuffer {
     }
 };
 
-void consumer(int id, BoundedBuffer& buffer){
+//void consumer(int id, BoundedBuffer& buffer){
+void consumer(int id, BoundedBuffer<200>& buffer){
     for(int i = 0; i < 50; ++i)
     {
         int value = buffer.pop();
@@ -122,7 +148,8 @@ void consumer(int id, BoundedBuffer& buffer){
     }
 }
 
-void producer(int id, BoundedBuffer& buffer){
+//void producer(int id, BoundedBuffer& buffer){
+void producer(int id, BoundedBuffer<200>& buffer){
     for(int i = 0; i < 75; ++i)
     {
         buffer.push(i);
@@ -134,7 +161,8 @@ void producer(int id, BoundedBuffer& buffer){
 }
 
 int main(){
-    BoundedBuffer buffer(200);
+    //BoundedBuffer buffer(200);
+    BoundedBuffer<200> buffer(200);
 
     std::thread c1(consumer, 0, std::ref(buffer));
     std::thread c2(consumer, 1, std::ref(buffer));
