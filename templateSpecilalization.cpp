75d8ebff72a04790c3ea9de01d14406149b24be1
wintersnow <kobesummerrain@gmail.com>
2020-01-01 23:25:02 -0500

    Update templateSpecilalization.cpp

--- templateSpecilalization.cpp
+++ templateSpecilalization.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 using namespace std;
+
+// Function specialization can not be partial speciliazation
 /****************************************************************
 *  Non-type template parameter
 ****************************************************************/
