7200bf931b9f45447bb15753b8c8b03993b9b04b
wintersnow <kobesummerrain@gmail.com>
2020-01-15 23:36:53 -0500

    Update memoryPool.cpp

--- memoryPool.cpp
+++ memoryPool.cpp
@@ -1,2 +1,4 @@
 https://zhuanlan.zhihu.com/p/64719710
 https://github.com/double-free/tiny-projects/blob/master/memory-pool/MemoryPool.h
+https://www.zhihu.com/question/25527491
+https://zhuanlan.zhihu.com/p/51855842
