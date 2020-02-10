16f8f550605cb6bb7eb6c4441a4f0a7c296a3492
wintersnow212 <kobesummerrain@gmail.com>
2020-02-10 13:07:36 -0500

    remove duplicate

---
+++ Memory/Slaballocation.cpp
@@ -0,0 +1,6 @@
+/* Slab allocation is a memory management mechanism intended for the efficient memory allocation of objects. 
+Compared to earlier mechanisms, it reduces fragmentation caused by allocations and deallocations. 
+The technique is used to retain allocated memory that contains a data object of a certain type for reuse upon subsequent allocations of objects of the same type. 
+It is analogous to an object pool, but only applies to memory, not other resources.
+*/
+
