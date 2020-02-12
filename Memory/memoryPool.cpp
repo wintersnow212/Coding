ee3d9c60deee946df9b72fdd57fd4fde4d4b91d8
wintersnow <kobesummerrain@gmail.com>
2020-02-12 17:33:33 -0500

    Update memoryPool.cpp

--- Memory/memoryPool.cpp
+++ Memory/memoryPool.cpp
@@ -8,3 +8,16 @@ efficient c++ programing techniques book
 
 http://dmitrysoshnikov.com/compilers/writing-a-memory-allocator/
 http://dmitrysoshnikov.com/compilers/writing-a-pool-allocator/
+https://github.com/mtrebi/memory-allocators
+
+memory pool 是allocate 一大块memory 减少 malloc的次数 但是缺点是就是固定长度了 
+不像malloc是dynmaic size 的所以他有什么first fit best fit stragety 
+
+感觉memory pool 就是像stack一般的linked list就可以 因为他不需要是sorted linked list 所以有free block就insert到头
+malloc比较复杂 他要在中间insert 所以用doubly linked list 比较好
+
+union的用法比较疑惑 https://thinkingeek.com/2017/11/19/simple-memory-pool/ 
+但是感觉其实就是为了return 自身地址
+
+linked list node structure其实不需要data 只需要node* next;
+其实node本身就是chunk的首地址 加上next的辅助把间断的chunk都连起来了！！！
