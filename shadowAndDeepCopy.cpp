6b70894619e4c037f57ef285fccd98b6051b10c9
wintersnow <kobesummerrain@gmail.com>
2020-02-27 15:47:45 -0500

    Update shadowAndDeepCopy.cpp

--- shadowAndDeepCopy.cpp
+++ shadowAndDeepCopy.cpp
@@ -1,44 +1,45 @@
-#include <cassert>
 #include <iostream>
-#include <cstring> // for strlen()
-#include <cassert> // for assert()
-#include <iostream>
-
+#include <cstring>
 using namespace std;
+
 class MyString
-{ 
+{
 public:
     MyString()
         : m_data(nullptr)
-        , m_length(0)
+        , m_len(0)
     {
-        
+
     }
-    MyString(const char* source)
+    MyString(const char* data)
     {
-        assert(source != nullptr); // make sure source isn't a null string
- 
-        // Find the length of the string
-        // Plus one character for a terminator
-        m_length = std::strlen(source) + 1;
+        // 这里一定要+1！！！！
+        // strlen: Returns the length of the given byte string not including null terminator;
+        m_len = strlen(data) + 1;
+        //m_len = strlen(data);
         
         // 这里必须要class 自己hold memory 不然要是shadow copy的pointer被free了呢
         // Allocate a buffer equal to this length
-        m_data = new char[m_length];
-        
-//         // Copy the parameter string into our internal buffer
-//         for (int i=0; i < m_length; ++i)
-//             m_data[i] = source[i];
-    
-//         // Make sure the string is terminated
-//         m_data[m_length-1] = '\0';
-        //strcpy(m_data, source);
-        memcpy(m_data, source, sizeof(char) * m_length);
+        m_data = new char[m_len];
+        strcpy(m_data, data);
+        memcpy(m_data, data, sizeof(char) * m_len);
+        //cout << m_data << endl;
+        // Do I need to add null terminator here??? No !!!
     }
+
+    /*****************************************************************************************************
+    Shadow copy
+    ******************************************************************************************************/
+    // MyString(const MyString& other)
+    //     : m_data(other.m_data)
+    //     , m_len(other.m_len)
+    // {
+
+    // }
+
+    /*****************************************************************************************************
+    Deep copy --- copy constructor
     
-    
-    // Deep copy --- copy constructor
-    /*
     Copy constructor is called when a new object is created from an existing     
     object, as a copy of the existing object (see this G-Fact). 
     And assignment operator is called when an already initialized object is 
@@ -54,37 +55,24 @@ public:
     C++ will provide a default copy constructor and default assignment operator that do a shallow copy.
     默认的是浅拷贝  所以自己写的那个copy constructor以及overload = operator就是深拷贝 
     Doing deep copies requires that we write our own copy constructors and overloaded assignment operators.
-    */
-    MyString(const MyString& source)
+    ******************************************************************************************************/
+    MyString(const MyString& other)
     {
-        m_length = source.m_length;
-        // copy assignment 才需要free
+        cout << "Copy constructor called" << endl;
+        // copy assignment 才需要free!!!!!!!!
         // if (m_data != nullptr)
         // {
         //     delete [] m_data;
         //     m_data = nullptr;
         // }
-            
-        // m_data is a pointer, so we need to deep copy it if it is non-null
-        if (source.m_data)
-        {
-            // allocate memory for our copy
-            m_data = new char[m_length];
+        m_len  = other.m_len;
+        m_data = new char[m_len];
 
-            // do the copy
-            // for (int i=0; i < m_length; ++i)
-            //     m_data[i] = source.m_data[i];
-            strcpy(m_data, source.m_data);
-        }
-        else
-        {
-            m_data = nullptr;
-        }
-
+        memcpy(m_data, other.m_data, sizeof(char) * m_len);
     }
 
-    // Deep Overload = operator
-    // Return by reference
+    /*****************************************************************************************************
+    Return by reference
     /*
     1.可以作为左值的原因
         Return value:此时生成的是变量a的一个拷贝，即生成了一个临时变量，当这个变量使用完毕之后，变量就被销毁了，
@@ -117,75 +105,108 @@ public:
         ....
         return temp; // copy constructor called
     }
-    */
-    MyString& operator = (const MyString & source)
+    *****************************************************************************************************/
+
+    MyString& operator = (const MyString& other)
     {
-        cout << "Deep = operator overload is called" << endl;
-        // check for self-assignment
-        // 注意 这里不能是*this  == source 因为没有== operator
-        if (this == &source)
-            return *this;
-
-        // first we need to deallocate any value that this string is holding!
-        // 这一步非常关键！！！！！
-        if (m_data != nullptr)
-        {
-            delete[] m_data;
-            m_data = nullptr;
-        }
-        
-        // because m_length is not a pointer, we can shallow copy it
-        m_length = source.m_length;
-
-        // m_data is a pointer, so we need to deep copy it if it is non-null
-        if (source.m_data)
-        {
-            // allocate memory for our copy
-            m_data = new char[m_length];
-
-            // do the copy
-            // ???? strcpy(s, src.s); ??? memcpy???
-            // for (int i=0; i < m_length; ++i)
-            //     m_data[i] = source.m_data[i];
-            // strlen: Returns the length of the given byte string not including null terminator;
-            memcpy(m_data, source.m_data, strlen(source.m_data) + 1);
-            //memcpy(m_data, source.m_data, sizeof(char) * m_length);
-        }
-        else
-            m_data = nullptr;
+        cout << "Copy Assignment called" << endl;
+        // Copy ans Swap idom
+        MyString cpy = other;
+        swap(m_data, cpy.m_data);
+        swap(m_len, cpy.m_len);
 
         return *this;
     }
-    ~MyString() // destructor
-    {
-        // We need to deallocate our string
-        // 如果是用new char [] 就得用delete [] m_data
-        delete [] m_data;
-    }
- 
-    char* getString() { return m_data; }
-    int getLength() { return m_length; }
+
+//     MyString& operator = (const MyString & source)
+//     {
+//         cout << "Deep = operator overload is called" << endl;
+//         // Check for self-assignment
+//         // 注意 这里不能是*this  == source 因为没有== operator
+//         if (this == &source)
+//             return *this;
+
+//         // first we need to deallocate any value that this string is holding!
+//         // 这一步非常关键！！！！！
+//         if (m_data != nullptr)
+//         {
+//             delete[] m_data;
+//             m_data = nullptr;
+//         }
+        
+//         // because m_length is not a pointer, we can shallow copy it
+//         m_length = source.m_length;
+
+//         // m_data is a pointer, so we need to deep copy it if it is non-null
+//         if (source.m_data)
+//         {
+//             // allocate memory for our copy
+//             m_data = new char[m_length];
+
+//             // do the copy
+//             memcpy(m_data, source.m_data, strlen(source.m_data) + 1);
+//             //memcpy(m_data, source.m_data, sizeof(char) * m_length);
+//         }
+//         else
+//             m_data = nullptr;
+
+//         return *this;
+//     }
     
+    
+    // Move constructor
+    MyString (MyString&& other)
+        : m_data(other.m_data)
+        , m_len (other.m_len)
+    {
+        // move 是相当于 让另外一个指针指向他 
+        // 所以当然不能delete了， delete之后原来指向他的pointer怎么办！！！
+        other.m_data = nullptr;
+        other.m_len  = 0;
+    }
+
+    ~ MyString()
+    {
+        if (m_data != nullptr)
+        {
+            // 如果是用new char [] 就得用delete [] m_data
+            delete [] m_data;
+            m_data = nullptr;
+        }
+    }
+    char* getData()
+    {
+        return m_data;
+    }
+
+    int getLen()
+    {
+        return m_len;
+    }
 private:
     char* m_data;
-    int m_length;
+    int m_len;
 };
 
 int main()
 {
-    MyString hello("Hello, world!");
-    
+    MyString test ("Hello World");
     {
-        MyString copy = hello; // use default copy constructor
+        MyString localTemp(test);
     }
+    // Copy is a local variable, so it gets destroyed here.  
+    // The destructor deletes copy's string, which leaves hello with a dangling pointer
+
+    cout << test.getData() << endl;
+    MyString assign("Hi Earch");
+    // MyString assign;
+    // assign = test;
+    // cout << assign.getData() << endl;
+    // 这里要想调用copy assignment 必须是两个已经created的object
+    // empty object i.e.  MyString assign; 也算是created!!!!
+    test = assign;
+    cout << test.getData() << endl;
     
-    // copy is a local variable, so it gets destroyed here.  
-    //The destructor deletes copy's string, which leaves hello with a dangling pointer
- 
-    std::cout << hello.getString() << '\n'; // this will have undefined behavior
-    
-    MyString assign;
-    assign = hello;
     
     return 0;
 }
