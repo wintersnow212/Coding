693c7fa52a73f2d6074c862c5d34c952dd2469d2
wintersnow <kobesummerrain@gmail.com>
2020-03-02 11:31:02 -0500

    Update MoveMemoryBlock.cpp

--- MoveMemoryBlock.cpp
+++ MoveMemoryBlock.cpp
@@ -10,6 +10,15 @@ using namespace std;
 //https://www.learncpp.com/cpp-tutorial/15-3-move-constructors-and-move-assignment/
 // 可能需要disable copy constructor
 // copy&&move construct 不用free 但是感觉copy assignment 和move assignment需要
+
+A move constructor is called:
+
+when an object initializer is std::move(something)
+when an object initializer is std::forward<T>(something) and T is not an lvalue reference type (useful in template programming for "perfect forwarding")
+when an object initializer is a temporary and the compiler doesn't eliminate the copy/move entirely
+when returning a function-local class object by value and the compiler doesn't eliminate the copy/move entirely
+when throwing a function-local class object and the compiler doesn't eliminate the copy/move entirely
+
 ****************************************************************************************/
 
 template<typename T>
