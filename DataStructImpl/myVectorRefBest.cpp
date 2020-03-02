457a08ec33356d52c94991ca64cb9c474b2fdbe3
wintersnow <kobesummerrain@gmail.com>
2020-03-02 00:54:57 -0500

    Create myVectorRefBest.cpp

---
+++ DataStructImpl/myVectorRefBest.cpp
@@ -0,0 +1,153 @@
+// https://lokiastari.com/blog/2016/03/12/vector-resize/index.html
+// https://lokiastari.com/blog/2016/03/19/vector-simple-optimizations/index.html
+template<typename T>
+class Vector
+{
+    std::size_t     capacity;
+    std::size_t     length;
+    T*              buffer;
+
+    struct Deleter
+    {
+        void operator()(T* buffer) const
+        {
+            ::operator delete(buffer);
+        }
+    };
+
+    public:
+        Vector(int capacity = 10)
+            : capacity(capacity)
+            , length(0)
+            , buffer(static_cast<T*>(::operator new(sizeof(T) * capacity)))
+        {}
+        ~Vector()
+        {
+            // Make sure the buffer is deleted even with exceptions
+            // This will be called to release the pointer at the end
+            // of scope.
+            std::unique_ptr<T, Deleter>     deleter(buffer, Deleter());
+
+            // Call the destructor on all the members in reverse order
+            for(int loop = 0; loop < length; ++loop)
+            {
+                // Note we destroy the elements in reverse order.
+                buffer[length - 1 - loop].~T();
+            }
+        }
+        Vector(Vector const& copy)
+            : capacity(copy.length)
+            , length(0)
+            , buffer(static_cast<T*>(::operator new(sizeof(T) * capacity)))
+        {
+            try
+            {
+                for(int loop = 0; loop < copy.length; ++loop)
+                {
+                    push_back(copy.buffer[loop]);
+                }
+            }
+            catch(...)
+            {
+                std::unique_ptr<T, Deleter>     deleter(buffer, Deleter());
+                // If there was an exception then destroy everything
+                // that was created to make it exception safe.
+                for(int loop = 0; loop < length; ++loop)
+                {
+                    buffer[length - 1 - loop].~T();
+                }
+
+                // Make sure the exceptions continue propagating after
+                // the cleanup has completed.
+                throw;
+            }
+        }
+        Vector& operator=(Vector const& copy)
+        {
+            // Copy and Swap idiom
+            Vector<T>  tmp(copy);
+            tmp.swap(*this);
+            return *this;
+        }
+        Vector(Vector&& move) noexcept
+            : capacity(0)
+            , length(0)
+            , buffer(nullptr)
+        {
+            move.swap(*this);
+        }
+        Vector& operator=(Vector&& move) noexcept
+        {
+            move.swap(*this);
+            return *this;
+        }
+        void swap(Vector& other) noexcept
+        {
+            using std::swap;
+            swap(capacity,      other.capacity);
+            swap(length,        other.length);
+            swap(buffer,        other.buffer);
+        }
+        void push_back(T const& value)
+        {
+            resizeIfRequire();
+            pushBackInternal(value);
+        }
+        void pop_back()
+        {
+            --length;
+            buffer[length].~T();
+        }
+        void reserve(std::size_t capacityUpperBound)
+        {
+            if (capacityUpperBound > capacity)
+            {
+                reserveCapacity(capacityUpperBound);
+            }
+        }
+    private:
+        void resizeIfRequire()
+        {
+            if (length == capacity)
+            {
+                std::size_t     newCapacity  = std::max(2.0, capacity * 1.5);
+                reserveCapacity(newCapacity);
+            }
+        }
+        void reserveCapacity(std::size_t newCapacity)
+        {
+            Vector<T>  tmpBuffer(newCapacity);
+
+            simpleCopy<T>(tmpBuffer);
+
+            tmpBuffer.swap(*this);
+        }
+        void pushBackInternal(T const& value)
+        {
+            new (buffer + length) T(value);
+            ++length;
+        }
+        void moveBackInternal(T&& value)
+        {
+            new (buffer + length) T(std::move(value));
+            ++length;
+        }
+
+        template<typename X>
+        typename std::enable_if<std::is_nothrow_move_constructible<X>::value == false>::type
+        simpleCopy(Vector<T>& dst)
+        {
+            std::for_each(buffer, buffer + length,
+                          [&dst](T const& v){dst.pushBackInternal(v);}
+                         );
+        }
+
+        template<typename X>
+        typename std::enable_if<std::is_nothrow_move_constructible<X>::value == true>::type
+        simpleCopy(Vector<T>& dst)
+        {
+            std::for_each(buffer, buffer + length,
+                          [&dst](T& v){dst.moveBackInternal(std::move(v));}
+                         );
+        }
+};
